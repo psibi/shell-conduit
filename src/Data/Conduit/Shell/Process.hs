53ba9f20184e1827f7afeb54fb901ce417918ecd
Chris Done <chrisdone@gmail.com>
2014-09-20 20:53:21 +0200

    Reformat, no semantic change

--- src/Data/Conduit/Shell/Process.hs
+++ src/Data/Conduit/Shell/Process.hs
@@ -112,59 +112,57 @@ conduitProcess
   :: (MonadResource m)
      => CreateProcess
      -> Conduit Chunk m Chunk
-conduitProcess cp = bracketP createp closep $ \(Just cin, Just cout, _, ph) -> do
-  end <- repeatLoopT $ do
-    -- if process's outputs are available, then yields them.
-    repeatLoopT $ do
-      b <- liftIO $ hReady' cout
-      when (not b) exit
-      out <- liftIO $ S.hGetSome cout bufSize
-      void $ lift . lift $ yield (Right out)
-
-    -- if process exited, then exit
-    end <- liftIO $ getProcessExitCode ph
-    when (isJust end) $ exitWith end
-
-    inp <- lift await
-    case inp of
-      -- if upper stream ended, then exit
-      Nothing -> exitWith Nothing
-      Just c ->
-        case c of
-          -- pass along errors to next process
-          Left{} -> lift (leftover c)
-          -- write stdin into this process
-          Right s ->
-             liftIO (do S.hPut cin s
-                        hFlush cin)
-
-  -- uppstream or process is done.
-  -- process rest outputs.
-  liftIO $ hClose cin
-  repeatLoopT $ do
-    out <- liftIO $ S.hGetSome cout bufSize
-    when (S.null out) exit
-    lift $ yield (Right out)
-
-  ec <- liftIO $ maybe (waitForProcess' ph) return end
-  case ec of
-    ExitSuccess -> return ()
-    ExitFailure i -> lift (monadThrow (ShellExitFailure i))
-
-  where
-    createp = createProcess cp
-      { std_in  = CreatePipe
-      , std_out = CreatePipe
-      }
-
-    closep (Just cin, Just cout, _, ph) = do
-      hClose cin
-      hClose cout
-      _ <- waitForProcess' ph
-      return ()
-    closep _ = error "Data.Conduit.Process.closep: Unhandled case"
-
-    hReady' h =
-      hReady h `E.catch` \(E.SomeException _) -> return False
-    waitForProcess' ph =
-      waitForProcess ph `E.catch` \(E.SomeException _) -> return ExitSuccess
+conduitProcess cp =
+  bracketP createp
+           closep
+           (\(Just cin,Just cout,Just cerr,ph) ->
+              do let proxy =
+                       do b <- liftIO (hReady' cout)
+                          when (not b) exit
+                          out <- liftIO (S.hGetSome cout bufSize)
+                          (void . lift . lift) (yield (Right out))
+                 end <- repeatLoopT
+                          (do repeatLoopT proxy
+                              end <- liftIO (getProcessExitCode ph)
+                              when (isJust end)
+                                   (exitWith end)
+                              inp <- lift await
+                              case inp of
+                                Nothing ->
+                                  exitWith Nothing
+                                Just c ->
+                                  case c of
+                                    Left{} ->
+                                      lift (leftover c)
+                                    Right s ->
+                                      liftIO (do S.hPut cin s
+                                                 hFlush cin))
+                 liftIO (hClose cin)
+                 repeatLoopT
+                   (do out <- liftIO (S.hGetSome cout bufSize)
+                       when (S.null out) exit
+                       lift (yield (Right out)))
+                 ec <- liftIO (maybe (waitForProcess' ph) return end)
+                 case ec of
+                   ExitSuccess -> return ()
+                   ExitFailure i ->
+                     lift (monadThrow (ShellExitFailure i)))
+  where createp =
+          createProcess
+            cp {std_in = CreatePipe
+               ,std_out = CreatePipe
+               ,std_err = CreatePipe}
+        closep (Just cin,Just cout,_,ph) =
+          do hClose cin
+             hClose cout
+             _ <- waitForProcess' ph
+             return ()
+        closep _ =
+          error "Data.Conduit.Process.closep: Unhandled case"
+        hReady' h =
+          hReady h `E.catch`
+          \(E.SomeException _) -> return False
+        waitForProcess' ph =
+          waitForProcess ph `E.catch`
+          \(E.SomeException _) ->
+            return ExitSuccess
