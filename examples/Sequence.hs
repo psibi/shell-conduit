f09a7ccc143b4d465f4644ed477686be96140b91
Chris Done <chrisdone@gmail.com>
2014-09-29 20:37:47 +0200

    Remove bad example

--- examples/Sequence.hs
+++ examples/Sequence.hs
@@ -1,5 +1,4 @@
 import Data.Conduit.Shell
 
 main = do run (do shell "echo sup"; shell "echo hi")
-          run (do shell "echo sup"; sed "s/u/a/"; shell "echo hi")
           run (do shell "echo sup" $| sed "s/u/a/"; shell "echo hi")
