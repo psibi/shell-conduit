af2499c08932ff8162e45348aa4885a2447bf6bd
Sibi Prabakaran <sibi@psibi.in>
2017-03-05 10:57:33 +0530

    Add variadic list support

--- src/Data/Conduit/Shell/Variadic.hs
+++ src/Data/Conduit/Shell/Variadic.hs
@@ -2,24 +2,25 @@
 {-# LANGUAGE TypeFamilies #-}
 
 -- | Variadic process calling.
-
 module Data.Conduit.Shell.Variadic
-  (ProcessType(..)
-  ,variadicProcess
-  ,CmdArg(..))
-  where
+  ( ProcessType(..)
+  , variadicProcess
+  , CmdArg(..)
+  ) where
 
 import qualified Data.ByteString as SB
 import qualified Data.ByteString.Lazy as LB
-import           Data.Conduit.Shell.Process
+import Data.Conduit.Shell.Process
 import qualified Data.Text as ST
 import qualified Data.Text.Encoding as ST
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.List as L
 
 -- | A variadic process maker.
-variadicProcess :: (ProcessType r)
-                => String -> r
+variadicProcess
+  :: (ProcessType r)
+  => String -> r
 variadicProcess name = spr name []
 
 -- | Make the final conduit.
@@ -27,31 +28,38 @@ makeProcessLauncher :: String -> [ST.Text] -> Segment ()
 makeProcessLauncher name args = proc name (map ST.unpack args)
 
 -- | Process return type.
-class ProcessType t where
-    spr :: String -> [ST.Text] -> t
+class ProcessType t  where
+  spr :: String -> [ST.Text] -> t
 
-instance (r ~ ()) => ProcessType (Segment r) where
-    spr name args = makeProcessLauncher name (reverse args)
+instance (r ~ ()) =>
+         ProcessType (Segment r) where
+  spr name args = makeProcessLauncher name (reverse args)
 
 -- | Accept strings as arguments.
-instance (ProcessType r,CmdArg a) => ProcessType (a -> r) where
-    spr name args = \a -> spr name (toTextArg a : args)
+instance (ProcessType r, CmdArg a) =>
+         ProcessType (a -> r) where
+  spr name args = \a -> spr name (toTextArg a ++ args)
 
 -- | Command line argument.
 class CmdArg a  where
-  toTextArg :: a -> ST.Text
+  toTextArg :: a -> [ST.Text]
 
 instance CmdArg ST.Text where
-  toTextArg = id
+  toTextArg = pure . id
 
 instance CmdArg LT.Text where
-  toTextArg = LT.toStrict
+  toTextArg = pure . LT.toStrict
 
 instance CmdArg SB.ByteString where
-  toTextArg = ST.decodeUtf8
+  toTextArg = pure . ST.decodeUtf8
 
 instance CmdArg LB.ByteString where
-  toTextArg = LT.toStrict . LT.decodeUtf8
+  toTextArg = pure . LT.toStrict . LT.decodeUtf8
 
 instance CmdArg String where
-  toTextArg = ST.pack
+  toTextArg = pure . ST.pack
+
+instance CmdArg [String] where
+  toTextArg = map ST.pack
+  -- instance CmdArg [String] where
+--   toTextArg args = toTextArg $ L.concat $ L.intersperse (" ") args
