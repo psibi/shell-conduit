72df1dd2d8005acde1ff0170cf5a160f11c05b2c
Sibi Prabakaran <sibi@psibi.in>
2017-03-05 11:31:12 +0530

    Change cd implementation based on cmdArg

--- src/Data/Conduit/Shell/PATH.hs
+++ src/Data/Conduit/Shell/PATH.hs
@@ -1,24 +1,29 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# OPTIONS_GHC -fno-warn-missing-signatures -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC
+  -fno-warn-missing-signatures -fno-warn-unused-imports #-}
 
 -- | All binaries in PATH.
-
 module Data.Conduit.Shell.PATH where
 
-import           Control.Monad
-import           Control.Monad.IO.Class
-import           Data.Conduit.Shell.Process
-import           Data.Conduit.Shell.TH
-import           Data.Conduit.Shell.Variadic
-import           Data.List
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Conduit.Shell.Process
+import Data.Conduit.Shell.TH
+import Data.Conduit.Shell.Variadic
+import Data.List
 import qualified Data.Text as T (unpack)
-import           Prelude
-import           System.Directory
+import Prelude
+import System.Directory
 
 -- | Helpful CD command.
-cd :: (MonadIO m,CmdArg arg) => arg -> m ()
-cd fp = liftIO (setCurrentDirectory (T.unpack (toTextArg fp)))
+cd
+  :: (MonadIO m, CmdArg arg)
+  => arg -> m ()
+cd fp =
+  case (toTextArg fp) of
+    [] -> return ()
+    (path:_) -> liftIO $ setCurrentDirectory (T.unpack path)
 
 $(generateBinaries)
