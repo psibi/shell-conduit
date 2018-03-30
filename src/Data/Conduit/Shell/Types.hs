726647348d21eac2da19c77eb88f0e7a98dc8bf6
Sibi Prabakaran <sibi@psibi.in>
2018-03-30 23:48:23 +0530

    Fix for conduit rewrite

--- src/Data/Conduit/Shell/Types.hs
+++ src/Data/Conduit/Shell/Types.hs
@@ -11,61 +11,40 @@
 {-# LANGUAGE CPP #-}
 
 -- | All types.
-
 module Data.Conduit.Shell.Types where
 
 import Control.Applicative
-import Control.Exception
+import UnliftIO.Exception
 import Control.Monad
 import Control.Monad.Base
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Control.Monad.Trans.Control
 import Control.Monad.Trans.Resource
 import Data.Conduit
 import Data.Typeable
 
 -- | Shell transformer.
-newtype ShellT m a =
-  ShellT {runShellT :: ResourceT m a}
-  deriving (Applicative,Monad,Functor,MonadThrow,MonadIO,MonadTrans)
+newtype ShellT m a = ShellT
+  { runShellT :: ResourceT m a
+  } deriving (Applicative, Monad, Functor, MonadThrow, MonadIO, MonadTrans)
 
-deriving instance (MonadResourceBase m) => MonadBase IO (ShellT m)
-deriving instance (MonadResourceBase m) => MonadResource (ShellT m)
-
-#if MIN_VERSION_monad_control(1,0,0)
-newtype StMShell m a = StMShell{unStMGeoServer :: StM (ResourceT m) a}
-#endif
-
--- | Dumb instance.
-instance (MonadThrow m,MonadIO m,MonadBaseControl IO m) => MonadBaseControl IO (ShellT m) where
-#if MIN_VERSION_monad_control(1,0,0)
-  type StM (ShellT m) a = StMShell m a
-#else
-  newtype StM (ShellT m) a = StMShell{unStMGeoServer ::
-                                    StM (ResourceT m) a}
-#endif
-  liftBaseWith f =
-    ShellT (liftBaseWith
-              (\run ->
-                 f (liftM StMShell .
-                    run .
-                    runShellT)))
-  restoreM = ShellT . restoreM . unStMGeoServer
+deriving instance (MonadUnliftIO m) => MonadResource (ShellT m)
 
 -- | Intentionally only handles 'ShellException'. Use normal exception
 -- handling to handle usual exceptions.
-instance (MonadBaseControl IO (ShellT m),Applicative m,MonadThrow m) => Alternative (ConduitM i o (ShellT m)) where
-  empty = monadThrow ShellEmpty
-  x <|> y =
-    do r <- tryC x
-       case r of
-         Left (_ :: ShellException) -> y
-         Right rr -> return rr
+instance (MonadUnliftIO (ShellT m), Applicative m, MonadThrow m) =>
+         Alternative (ConduitT i o (ShellT m)) where
+  empty = throwIO ShellEmpty
+  x <|> y = do
+    r <- tryC x
+    case r of
+      Left (_ :: ShellException) -> y
+      Right rr -> return rr
 
 -- | An exception resulting from a shell command.
 data ShellException
   = ShellEmpty -- ^ For 'mempty'.
   | ShellExitFailure !Int -- ^ Process exited with failure.
-  deriving (Typeable,Show)
+  deriving (Typeable, Show)
+
 instance Exception ShellException
